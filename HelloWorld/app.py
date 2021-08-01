e208d3464362564d3ead07ad8626c4ef23dabc91
Tim Dorau <scharka01@googlemail.com>
2021-08-01 15:19:57 +0200

    Test QLineEdit Widget

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,70 +1,50 @@
 import sys
-
-from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
-    QApplication,
-    QCheckBox,
-    QComboBox,
-    QDateEdit,
-    QDateTimeEdit,
-    QDial,
-    QDoubleSpinBox,
-    QFontComboBox,
-    QLabel,
-    QLCDNumber,
-    QLineEdit,
-    QMainWindow,
-    QProgressBar,
-    QPushButton,
-    QRadioButton,
-    QSlider,
-    QSpinBox,
-    QTimeEdit,
-    QVBoxLayout,
-    QWidget,
+    QMainWindow, QApplication,
+    QLabel, QCheckBox, QComboBox,  QLineEdit,
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
 )
+from PySide6.QtCore import Qt
 
 
-# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
+
     def __init__(self):
-        super().__init__()
+        super(MainWindow, self).__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("My App")
 
-        layout = QVBoxLayout()
-        widgets = [
-            QCheckBox,
-            QComboBox,
-            QDateEdit,
-            QDateTimeEdit,
-            QDial,
-            QDoubleSpinBox,
-            QFontComboBox,
-            QLCDNumber,
-            QLabel,
-            QLineEdit,
-            QProgressBar,
-            QPushButton,
-            QRadioButton,
-            QSlider,
-            QSpinBox,
-            QTimeEdit,
-        ]
+        widget = QLineEdit()
+        widget.setMaxLength(10)
+        widget.setPlaceholderText("Enter your text")
 
-        for w in widgets:
-            layout.addWidget(w())
+        #widget.setReadOnly(True) # uncomment this to make readonly
 
-        widget = QWidget()
-        widget.setLayout(layout)
+        widget.returnPressed.connect(self.return_pressed)
+        widget.selectionChanged.connect(self.selection_changed)
+        widget.textChanged.connect(self.text_changed)
+        widget.textEdited.connect(self.text_edited)
 
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
         self.setCentralWidget(widget)
 
 
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
+    def return_pressed(self):
+        print("Return pressed!")
+        self.centralWidget().setText("BOOM!")
 
+    def selection_changed(self):
+        print("Selection changed")
+        print(self.centralWidget().selectedText())
+
+    def text_changed(self, s):
+        print("Text changed...")
+        print(s)
+
+    def text_edited(self, s):
+        print("Text edited...")
+        print(s)
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
 app.exec_()
